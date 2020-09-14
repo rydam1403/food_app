6ca7c9ffcffce88d7d8914752fe4d594ec7bacee
rydam1403 <rydam1911007@akgec.ac.in>
2020-09-15 02:06:03 +0530

    Food App

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -26,7 +28,6 @@ class MyApp extends StatelessWidget {
                   SizedBox(
                     height: 8.0,
                   ),
-
                   Container(
                     alignment: Alignment.centerLeft,
                     margin: EdgeInsets.only(left: 25.0),
@@ -43,47 +44,44 @@ class MyApp extends StatelessWidget {
                   ),
                   Column(
                     children: <Widget>[
-                      Fieldinput(type: 'UserName', texttype: TextInputType.text),
-                      Fieldinput(type: 'Email', texttype: TextInputType.emailAddress),
-                      Fieldinput(type: 'Password',texttype: TextInputType.visiblePassword),
-                      Fieldinput(type: 'Phone',texttype: TextInputType.number)
+                      Fieldinput(
+                          type: 'UserName', texttype: TextInputType.text),
+                      Fieldinput(
+                          type: 'Email', texttype: TextInputType.emailAddress),
+                      Fieldinput(
+                          type: 'Password',
+                          texttype: TextInputType.visiblePassword),
+                      Fieldinput(type: 'Phone', texttype: TextInputType.number)
                     ],
                   ),
-
                   SizedBox(
                     height: 27.0,
                   ),
-
                   Container(
-                    margin: EdgeInsets.only(left: 50.0,right: 50.0),
-                      child: RaisedButton(
-                          color: Colors.deepPurpleAccent,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)
-                          ),
-                          child: Text(
-                            'Sign UP',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                          onPressed: (){},
-                        elevation: 0.0,
+                    margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                    child: RaisedButton(
+                      color: Colors.deepPurpleAccent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      child: Text(
+                        'Sign UP',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
                       ),
+                      onPressed: () {},
+                      elevation: 0.0,
+                    ),
                   )
-
                 ],
-              )
-          )
-      ),
+              ))),
     );
   }
 
-  Widget Fieldinput({String type, TextInputType texttype}) =>
-      Container(
-        margin: EdgeInsets.only(left:20.0,right:20.0,bottom:7.7),
+  Widget Fieldinput({String type, TextInputType texttype}) => Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 7.7),
         child: Center(
           child: TextField(
             decoration: InputDecoration(
@@ -91,10 +89,9 @@ class MyApp extends StatelessWidget {
               fillColor: Colors.grey[300],
               labelText: '$type',
               labelStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
-                fontSize: 20.0
-              ),
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                  fontSize: 20.0),
               hintText: '$type',
               hintStyle: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -102,7 +99,7 @@ class MyApp extends StatelessWidget {
                 fontSize: 20.0,
               ),
               border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0)
+                borderRadius: BorderRadius.circular(30.0),
               ),
             ),
             keyboardType: texttype,
