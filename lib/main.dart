258f7164c6bb04ca21270c8f34a355aaa5cb65bd
rydam1403 <rydam1911007@akgec.ac.in>
2020-09-17 18:28:13 +0530

    Food App

--- lib/main.dart
+++ lib/main.dart
@@ -10,112 +10,128 @@ void main() {
       home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             backgroundColor: Colors.white,
-            body: ListView(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 30.0, bottom: 50.0),
-                  child: Center(
-                    child: Image(image: AssetImage('images/foodlogo.png')),
-                  ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(left: 25.0),
-                  child: Text(
-                    'Sign Up Free Account',
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 25.0,
-                ),
-                Column(
+            body: Form(
+                key: _formkey,
+                child: ListView(
                   children: <Widget>[
-                    Fieldinput(
-                      type: 'User Name',
-                      texttype: TextInputType.text,
-                    ),
-                    Fieldinput(
-                        type: 'Email', texttype: TextInputType.emailAddress),
-                    Fieldinput(
-                        type: 'Password',
-                        texttype: TextInputType.visiblePassword),
-                    Fieldinput(type: 'Phone', texttype: TextInputType.number)
-                  ],
-                ),
-                SizedBox(
-                  height: 27.0,
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 50.0, right: 50.0),
-                  child: RaisedButton(
-                    color: Colors.deepPurpleAccent,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
-                    child: Text(
-                      'Sign UP',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.white,
+                    Container(
+                      margin: EdgeInsets.only(top: 30.0, bottom: 50.0),
+                      child: Center(
+                        child: Image(image: AssetImage('images/foodlogo.png')),
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) {
-                          return HomePage();
-                        }),
-                      );
-                    },
-                    elevation: 0.0,
-                  ),
-                )
-              ],
-            )));
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.only(left: 25.0),
+                      child: Text(
+                        'Sign Up Free Account',
+                        style: TextStyle(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Fieldinput(
+                          type: 'User Name',
+                          texttype: TextInputType.text,
+                        ),
+                        Fieldinput(
+                            type: 'Email',
+                            texttype: TextInputType.emailAddress),
+                        Fieldinput(
+                            type: 'Password',
+                            texttype: TextInputType.visiblePassword),
+                        Fieldinput(
+                            type: 'Phone', texttype: TextInputType.number)
+                      ],
+                    ),
+                    SizedBox(
+                      height: 27.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                      child: RaisedButton(
+                        color: Colors.deepPurpleAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        child: Text(
+                          'Sign UP',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {
+                          if(_formkey.currentState.validate()){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) {
+                              return HomePage();
+                            }),
+                          );
+                        }},
+                        elevation: 0.0,
+                      ),
+                    )
+                  ],
+                ))));
   }
 
   Widget Fieldinput({String type, TextInputType texttype}) => Container(
         margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 7.7),
         child: Center(
-          child: TextField(
+          child: TextFormField(
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter the data';
+              }
+            },
             decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey[300],
-              enabledBorder: const OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(50.0)
+                filled: true,
+                fillColor: Colors.grey[300],
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
                 ),
-                borderSide: const BorderSide(color: Colors.grey,width: 0.0),
-              ),
-              labelText: '$type',
-              labelStyle: TextStyle(
+                labelText: '$type',
+                labelStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600],
+                    letterSpacing: 2.0,
+                    fontSize: 22.0),
+                hintText: '$type',
+                hintStyle: TextStyle(
                   fontWeight: FontWeight.bold,
-                  color: Colors.grey[600],
-                  letterSpacing: 2.0,
-                  fontSize: 22.0),
-              hintText: '$type',
-              hintStyle: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.grey,
-                fontSize: 20.0,
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30.0),
-              )
-            ),
+                  color: Colors.grey,
+                  fontSize: 20.0,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                )),
             keyboardType: texttype,
           ),
         ),
